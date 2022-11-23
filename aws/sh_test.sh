ac9d3aaca434d588674297c1fd50c870e2381797
Jonathan Jayes (Dell) <j0nathanjayes@gmail.com>
2022-11-23 16:31:22 +0100

    adding blekinge geocode

--- aws/sh_test.sh
+++ aws/sh_test.sh
@@ -1,5 +1,5 @@
 
 python aws/tptp.py "ingest/sodermanland/sodermanland_1.PNG"
-python aws/tptp.py "ingest/sodermanland/sodermanland_1.PNG"
-python aws/tptp.py "ingest/sodermanland/sodermanland_1.PNG"
-python aws/tptp.py "ingest/sodermanland/sodermanland_1.PNG"
+python aws/tptp.py "ingest/sodermanland/sodermanland_2.PNG"
+python aws/tptp.py "ingest/sodermanland/sodermanland_3.PNG"
+python aws/tptp.py "ingest/sodermanland/sodermanland_4.PNG"
